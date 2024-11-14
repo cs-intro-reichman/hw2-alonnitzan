eff70b108dbb09a5d7169e33cc04385466c29e31
alonnitzan <alon.nitzan@post.runi.ac.il>
2024-11-14 21:04:24 +0200

    Fixed Cheers

--- Cheers.java
+++ Cheers.java
@@ -12,7 +12,7 @@ public class Cheers {
                         char ch = word.charAt(i);
 
                         // checks if need 'an'
-                        if (ch == 'A' || ch == 'E' || ch == 'F' || ch == 'H' || ch == 'I' ||ch == 'L' || ch == 'M' ||ch == 'N' || ch == 'O' || ch == 'R'){
+                        if (ch == 'A' || ch == 'E' || ch == 'F' || ch == 'H' || ch == 'I' ||ch == 'L' || ch == 'M' ||ch == 'N' || ch == 'O' || ch == 'R' || ch == 'S' || ch == 'X'){
                                 System.out.println("Give me an " + ch + ": " + ch + "!");
                         }
                         else System.out.println("Give me a  " + ch + ": " + ch + "!");
